d7a1bc1f7cf769b4af5d55448f29bcd5275addc8
Katie Wilday <wildayk@gmail.com>
2019-06-21 19:42:11 -0400

    welcome view

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,2 +1,29 @@
 class TrainingsController < ApplicationController
+
+  def index
+    if current_user.specialty !=[]
+      @user_training = current_user.training
+      @training = training.all
+      @user_id = current_user.id
+    else
+      flash[:error] = "You need to pick a training session before you can choose an instructor."
+      redirect_to root_url
+    end
+  end
+
+  def create
+    @training = Training.create(user_id: current_user.id, instructor_id: params[:instructor_id], name: params[:instructor_training])
+    redirect_to user_training_path(current_user)
+  end
+
+  def show
+    @training_name = specialty_path.slice(10...)
+    @training_tutors = Instructor.training(@training_name)
+  end
+
+  def destroy
+    @training = Training.find(params[:id])
+    @training.delete
+    redirect_to user_training_path
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_21_225405) do
+ActiveRecord::Schema.define(version: 2019_06_21_230836) do
 
   create_table "instructors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trainings", force: :cascade do |t|
+    t.string "name"
+    t.time "day_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
